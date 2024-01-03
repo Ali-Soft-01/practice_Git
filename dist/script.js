3091001ac8deba9c4157bdc95c4149a78d467705
Ali-Soft-01 <alisoft1275@gmail.com>
2024-01-03 11:43:29 +0500

    uplpad first project

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+const closeBtn = document.getElementById("close-button");
+const bar = document.getElementById("bar");
+closeBtn.addEventListener("click", () => {
+    bar.classList.toggle("-top-96");
+})
